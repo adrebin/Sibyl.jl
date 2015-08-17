ad5464133d61e7a6ee765122fea3c1ce7d9046f8
Devin Kilminster <devin.kilminster@metservice.com>
2015-08-17 03:35:07 +0000

    Can read blocks now.

--- src/Sibyl.jl
+++ src/Sibyl.jl
@@ -12,6 +12,11 @@ typealias Bytes Array{UInt8,1}
 
 const empty=Bytes()
 
+function readstring(io)
+    l=read(io,Int16)
+    return UTF8String(read(io,Uint8,l))
+end
+
 function writebytes(io,x::String)
     b=IOBuffer()
     write(b,UTF8String(x))
@@ -66,6 +71,33 @@ function message(t::BlockTransaction)
     return Zlib.compress(r,9)
 end
 
+function interpret!(t::BlockTransaction,message::Bytes)
+    if length(message)==0
+        return
+    end
+    io=IOBuffer()
+    write(io,Zlib.decompress(message))
+    seekstart(io)
+    n=read(io,Int64)
+    for i=1:n
+        kl=read(io,Int64)
+        k=read(io,Uint8,kl)
+        vl=read(io,Int64)
+        v=read(io,Uint8,vl)
+        t.data[k]=v
+    end
+    n=read(io,Int64)
+    for i=1:n
+        kl=read(io,Int64)
+        k=read(io,Uint8,kl)
+        delete!(t.data,k)
+    end
+    n=read(io,Int64)
+    for i=1:n
+        push!(t.s3keystodelete,readstring(io))
+    end
+end
+
 type Transaction
     bucket::UTF8String
     space::UTF8String
@@ -102,6 +134,38 @@ function save(env,t::Transaction)
         end
     end
 end
+
+function s3listobjects(env,bucket,prefix)
+    [x.key for x in S3.get_bkt(env,bucket,options=GetBucketOptions(prefix=prefix)).obj.contents]
+end
+
+function s3getobject(env,bucket,s3key)
+    resp=S3.get_object(env,bucket,s3key)
+    if resp.http_code==200
+        return takebuf_array(resp.obj)
+    end
+    return empty
+end
+
+function readblock(env,bucket::UTF8String,space::UTF8String,table::UTF8String,key::Bytes)
+    objects=[(reinterpret(Int64,base64decode(ASCIIString(split(x,",")[5])))[1],
+              split(x,",")[6],x)
+             for x in s3listobjects(env,bucket,s3keyprefix(space,table,key))]
+    sort!(objects)
+    results=[]
+    @sync for obj in objects
+        r=RemoteRef()
+        push!(results,r)
+        @async put!(r,s3getobject(env,bucket,obj[3]))
+    end
+    r=BlockTransaction()
+    for result in results
+        interpret!(r,fetch(result))
+    end
+    return r
+end
+
+
 
 end
 
