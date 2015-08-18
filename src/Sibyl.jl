13d70f9f545ea305fc2459d553b78d4666e88584
Devin Kilminster <devin.kilminster@metservice.com>
2015-08-18 03:01:49 +0000

    Have an explicit connection object.

--- src/Sibyl.jl
+++ src/Sibyl.jl
@@ -4,9 +4,18 @@ using SHA
 using Zlib
 using AWS
 using AWS.S3
+using Memcache
 
 export asbytes,frombytes
 export empty
+
+type Connection
+    bucket::UTF8String
+    space::UTF8String
+    env::AWSEnv
+end
+
+Connection(bucket,space)=Connection(UTF8String(bucket),UTF8String(space),AWSEnv(timeout=60.0))
 
 typealias Bytes Array{UInt8,1}
 
@@ -113,12 +122,11 @@ function interpret!(t::BlockTransaction,message::Bytes)
 end
 
 type Transaction
-    bucket::UTF8String
-    space::UTF8String
+    connection::Connection
     tables::Dict{UTF8String,Dict{Bytes,BlockTransaction}}
 end
 
-Transaction(bucket,space)=Transaction(bucket,space,Dict{UTF8String,Dict{Bytes,BlockTransaction}}())
+Transaction(connection)=Transaction(connection,Dict{UTF8String,Dict{Bytes,BlockTransaction}}())
 
 function upsert!(t::Transaction,table::UTF8String,key::Bytes,subkey::Bytes,value::Bytes)
     if !(haskey(t.tables,table))
@@ -135,15 +143,15 @@ function s3keyprefix(space,table,key)
     sha256(base)[1:4]*base
 end
 
-function save(env,t::Transaction)
+function save(t::Transaction)
     @sync for (table,blocktransactions) in t.tables
         for (key,blocktransaction) in blocktransactions
-            s3prefix=s3keyprefix(t.space,table,key)
+            s3prefix=s3keyprefix(t.connection.space,table,key)
             m=message(blocktransaction)
-            timestamp=base64encode(Int64(round(time())))
+            timestamp=base64encode(asbytes(Int64(round(time()))))
             s3key="$(s3prefix),$(timestamp),$(sha256(m))"
             @async begin
-                S3.put_object(env,t.bucket,s3key,ASCIIString(m))
+                S3.put_object(t.connection.env,t.connection.bucket,s3key,ASCIIString(m))
             end
         end
     end
@@ -161,16 +169,16 @@ function s3getobject(env,bucket,s3key)
     return empty
 end
 
-function readblock(env,bucket::UTF8String,space::UTF8String,table::UTF8String,key::Bytes)
-    objects=[(reinterpret(Int64,base64decode(ASCIIString(split(x,",")[5])))[1],
+function readblock(connection::Connection,table::UTF8String,key::Bytes)
+    objects=[(frombytes(base64decode(ASCIIString(split(x,",")[5])),Int64)[1],
               split(x,",")[6],x)
-             for x in s3listobjects(env,bucket,s3keyprefix(space,table,key))]
+             for x in s3listobjects(connection.env,connection.bucket,s3keyprefix(connection.space,table,key))]
     sort!(objects)
     results=[]
     @sync for obj in objects
         r=RemoteRef()
         push!(results,r)
-        @async put!(r,s3getobject(env,bucket,obj[3]))
+        @async put!(r,s3getobject(connection.env,connection.bucket,obj[3]))
     end
     r=BlockTransaction()
     for result in results
@@ -179,12 +187,12 @@ function readblock(env,bucket::UTF8String,space::UTF8String,table::UTF8String,ke
     return r
 end
 
-function loadblocks!(env,t::Transaction,tablekeys)
+function loadblocks!(t::Transaction,tablekeys)
     results=[]
     @sync for (table,key) in tablekeys
         r=RemoteRef()
         push!(results,r)
-        @async put!(r,readblock(env,t.bucket,t.space,table,key))
+        @async put!(r,readblock(t.connection,table,key))
     end
     for i=1:length(tablekeys)
         if !haskey(t.tables,tablekeys[i][1])
