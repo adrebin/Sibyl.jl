3018f7799b602e8a6996ece8885f22a0ecf47be4
Will Catton <will.catton@metservice.com>
2016-02-04 01:13:52 +0000

    Added try..catch to sqlite calls

--- src/sqlitecache.jl
+++ src/sqlitecache.jl
@@ -15,29 +15,41 @@ end
 function Cache()
     home=ENV["HOME"]
     db=SQLite.DB("$(home)/.sibyl.sqlite")
-    SQLite.execute!(db,"""CREATE TABLE IF NOT EXISTS Cache (
-                    ky TEXT PRIMARY KEY,
-                    expires INTEGER,
-                    value BLOB)""")
-    SQLite.execute!(db,"""CREATE INDEX IF NOT EXISTS Cache_index ON Cache (ky)""")
-    SQLite.execute!(db,"""VACUUM""")
+    try
+        SQLite.execute!(db,"""CREATE TABLE IF NOT EXISTS Cache (
+                        ky TEXT PRIMARY KEY,
+                        expires INTEGER,
+                        value BLOB)""")
+        SQLite.execute!(db,"""CREATE INDEX IF NOT EXISTS Cache_index ON Cache (ky)""")
+        SQLite.execute!(db,"""VACUUM""")
+    catch
+    end
     lock=Base.Semaphore(1)
     return Cache(db,lock)
+
 end
 
 function writecache(cache::Cache,key::UTF8String,expiry::Int,data::Bytes)
     Base.acquire(cache.lock)
-    st=SQLite.Stmt(cache.db,"INSERT OR REPLACE INTO Cache (ky,expires,value) VALUES (?,?,?)")
-    SQLite.bind!(st,1,key)
-    SQLite.bind!(st,2,Int(round(time()))+expiry)
-    SQLite.bind!(st,3,data)
-    SQLite.execute!(st)
+    try
+        st=SQLite.Stmt(cache.db,"INSERT OR REPLACE INTO Cache (ky,expires,value) VALUES (?,?,?)")
+        SQLite.bind!(st,1,key)
+        SQLite.bind!(st,2,Int(round(time()))+expiry)
+        SQLite.bind!(st,3,data)
+        SQLite.execute!(st)
+    catch
+    end
     Base.release(cache.lock)
 end
 
 function readcache(cache::Cache,key::UTF8String)
     Base.acquire(cache.lock)
-    results=SQLite.query(cache.db,"SELECT expires,value FROM Cache where ky=?",[key])
+    try
+        results=SQLite.query(cache.db,"SELECT expires,value FROM Cache where ky=?",[key])
+    catch
+        Base.release(cache.lock)
+        return Nullable{Bytes}()
+    end
     Base.release(cache.lock)
 #    println(key)
     if size(results,1)==0 # If nothing was found
